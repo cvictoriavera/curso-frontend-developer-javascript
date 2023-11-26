0a04e4611cf2ae4afd02a323fc0d56cdc08f0a89
Victoria Vera <cvictoriavera@gmail.com>
2023-11-26 19:36:00 -0300

    Se agrego el menu desplegable para mobile y se arreglo lo del menu desktop

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerIcon = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
 
